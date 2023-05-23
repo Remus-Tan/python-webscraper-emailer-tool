3ccc29c7daeb4a84dbc260abe0bef0338ef35556
Remus <remusthings@gmail.com>
2023-05-23 23:52:15 +0800

    Change scheduler to every 4h, change promo code text to entire line, add local testing settings for chrome webdriver

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+# import chromedriver_autoinstaller
 import re
 
 options = Options()
@@ -11,6 +12,9 @@ options.add_argument('--disable-dev-shm-usage')
 options.add_argument('disable-infobars')
 options.add_argument("--disable-extensions")
 
+# For local testing
+# chromedriver_autoinstaller.install()
+# driver = webdriver.Chrome(chrome_options=options)
 driver = webdriver.Chrome(chrome_options=options, executable_path='/usr/bin/chromedriver')
 
 def scrape_myprotein():
@@ -20,10 +24,9 @@ def scrape_myprotein():
     myprotein_banner_text = driver.find_elements(By.CLASS_NAME, myprotein_banner_class)
 
     for child in myprotein_banner_text:
-        search = re.findall('\s\d*%\s', child.text)
+        search = re.findall('.*\s\d*%\s.*', child.text)
 
     for index, text in enumerate(search):
         search[index] = text.strip()
 
-    return search
-
+    return ' '.join(search)
